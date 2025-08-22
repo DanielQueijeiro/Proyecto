bb42b0bc81b76578a2ff8ada9c8bd10f4117567b
Daniel Queijeiro <dany.queijeiro@hotmail.com>
2025-08-22 17:50:03 -0600

    agregar vistas

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -30,6 +30,12 @@ const router = createRouter({
       meta: { requiresAuth: true },
     },
     {
+      path: '/analisis',
+      name: 'analisis',
+      component: () => import('../views/AnalisisView.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
       path: '/login',
       name: 'login',
       component: () => import('../views/LoginView.vue'),
---
+++ frontend/src/views/AnalisisView.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+function handleSubmit() {
+  // Cargar csv
+}
+    
+</script>
+
+<template>
+    <section class="container">
+        <div class="mb-3">
+            <label for="formFile" class="form-label">Sube tu archivo CSV</label>
+            <input class="form-control" type="file" id="formFile">
+        </div>
+          <div class="col-auto">
+            <button type="submit" class="btn btn-primary mb-3" @click="handleSubmit">Enviar archivo</button>
+        </div>
+    </section>
+</template>
